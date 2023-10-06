0acf1e386601fab6d4420e4432c0f731ae131f8e
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-10-06 10:00:19 +0530

    updated

--- mapfilterreduce.js
+++ mapfilterreduce.js
@@ -20,3 +20,40 @@ console.log(result) //['<p>1</p>', '<p>2</p>', '<p>3</p>', '<p>4</p>', '<p>5</p>
 //binary number
 var result = arr.map((x) => x.toString(2))
 console.log(result) //[ '1', '10', '11', '100', '101' ]
+
+//filter
+
+//used to filter out the value
+// it may or may not return the same length of output array as input array
+//only return those data which output or condition is true
+
+
+var arr = [1, 2, 3, 4, 5]
+
+const output = arr.filter((x) => x > 4)
+
+console.log(output)
+
+
+var farm = ["🐷", "🐮", "🐴", "🐐", "🐔"]
+
+const res = farm.filter((x) => x == "🐐")
+
+console.log(res)
+
+
+//reduce
+
+var arr = [5, 1, 3, 2, 6]
+
+//sum or max number 
+
+function findSum(arr) {
+    let sum = 0
+    for (var i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    return sum;
+}
+
+console.log(findSum(arr))
